4cb1606c619e3ffd32fc3564cd0960247777638f
amy61789 <amy61789@outlook.com>
2017-02-27 11:49:25 -0800

    Initial Commit

---
+++ CommunityAssistService/App_Code/CAService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CAService" in code, svc and config file together.
+public class CAService : ICAService
+{
+    Community_AssistEntities cae = new Community_AssistEntities();
+    public bool ApplyForGrant(GrantRequest gr)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<GrantInfo> GetGrantsByPerson(int personId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<GrantType> GetGrantTypes()
+    {
+        var types = from g in cae.GrantTypes
+                     select new
+                     {
+                         g.GrantTypeKey,
+                         g.GrantTypeName
+                     };
+        List<GrantType> gTypes = new List<GrantType>();
+        foreach(var t in types)
+        {
+            GrantType type = new GrantType();
+            type.GrantTypeKey = t.GrantTypeKey;
+            type.GrantTypeName = t.GrantTypeName;
+            gTypes.Add(type);
+        }
+        return gTypes;
+    }
+
+    public int PersonLogin(string user, string password)
+    {
+        int key = 0;
+        int result = cae.usp_Login(user, password);
+        if(result != -1)
+        {
+            var uKey = (from p in cae.People
+                       where p.PersonEmail.Equals(user)
+                       select p.PersonKey).FirstOrDefault();
+            key = (int)uKey;
+        }
+        return key;
+    }
+
+    public bool RegisterPerson(PersonLite p)
+    {
+        bool result = true;
+        int success = cae.usp_Register(p.LastName, p.FirstName, 
+            p.Email, p.Password, p.Apartment, p.Street, p.City, 
+            p.State, p.Zipcode, p.HomePhone, p.WorkPhone);
+        if (success == -1)
+            result = false;
+
+        return result;
+    }
+}
